a9e0f03b808aac66fe6c56c10ac6b6e5f7f2b0e2
Joe Jasinski <joe.jasinski@gmail.com>
2018-04-11 23:00:54 -0500

    Got user login to work

--- talkvoter/app.py
+++ talkvoter/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Talk, Vote
+from flask_login import LoginManager
+from .models import db, User, Talk, Vote
 from .v1_resources import api_bp
 from .config import Config
+from . import views
 
 
 application = Flask(__name__)
@@ -15,20 +17,27 @@ db.create_all(app=application)
 
 
 admin = Admin(application, name='flask4data', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Talk, db.session))
 admin.add_view(ModelView(Vote, db.session))
 
+login = LoginManager(application)
+login.login_view = 'login'
 
-@application.route("/")
-def index():
-    name = "Python"
-    return render_template('index.html', name=name)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @application.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
+
+application.add_url_rule(r'/login/', view_func=views.login, methods=['GET', 'POST'])
+application.add_url_rule(r'/logout/', view_func=views.logout, methods=['GET', 'POST'])
+application.add_url_rule(r'/', view_func=views.dashboard, methods=['GET', 'POST'])
 
 application.register_blueprint(
     api_bp,
--- talkvoter/models.py
+++ talkvoter/models.py
@@ -1,7 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+
 
 db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __str__(self):
+        return str(self.username)
 
 
 class Talk(db.Model):
@@ -23,7 +45,7 @@ class Talk(db.Model):
         return str(self.id)
 
     def __repr__(self):
-        return '<Talk %r>' % self.id
+        return '<Talk {}>'.format(self.id)
 
 
 class Vote(db.Model):
@@ -32,7 +54,7 @@ class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     talk_id = db.Column(db.Integer, db.ForeignKey('talk.id'))
     talk = relationship("Talk", backref="votes")
-    value = db.Column(db.String)
+    value = db.Column(db.Integer)  # 0=watch_later; 1=in_person
     processed = db.Column(db.Boolean, default=False)
 
     date_created = db.Column(
