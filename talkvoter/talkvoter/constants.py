adfa2b81038db370604acfc3977c8c82dd311314
Joe Jasinski <joe.jasinski@gmail.com>
2018-04-23 20:29:51 -0500

    filter /api/v1/predict/ endpoint to only return values of 1 (in_person)

--- talkvoter/talkvoter/constants.py
+++ talkvoter/talkvoter/constants.py
@@ -4,3 +4,8 @@ from enum import Enum
 class VoteValue(Enum):
     in_person = "in_person"
     watch_later = "watch_later"
+
+
+vote_mapping = {
+    VoteValue.in_person.value: 1,
+    VoteValue.watch_later.value: 0, }
